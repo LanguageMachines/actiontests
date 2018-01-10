ed87fc07ace23006797d21c494c4481c60badc11
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-01-10 14:58:37 +0100

    made OpenMP usage conditional in the config

--- src/main.cxx
+++ src/main.cxx
@@ -1,4 +1,7 @@
+#include "config.h"
+#ifdef HAVE_OPENMP
 #include "omp.h"
+#endif
 #include "textcat.h"
 #include <cmath>
 #include <string>
@@ -8,7 +11,13 @@ using namespace std;
 
 int main(){
   cerr << "start" << endl;
-  cerr << "OMP: num of threads=" << omp_get_num_threads() << endl;
+#ifdef HAVE_OPENMP
+  omp_set_num_threads(8);
+  cerr << "OMP: maximum number of threads=" << omp_get_max_threads() << endl;
+#else
+  cerr << "Too bad. No OpenMP support available! " << endl;
+#endif
+
   auto TC = textcat_Init( "dummy" );
   if ( TC != 0 ){
     cerr << "Surprise!" << endl;
