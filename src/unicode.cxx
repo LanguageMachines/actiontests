46cdbd37fc06033ab7737ba486f72541bec11936
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2024-08-08 16:29:15 +0200

    forgot a file

---
+++ src/unicode.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "unicode/ustream.h"
+#include "unicode/ustream.h"
+
+using namespace icu;
+using namespace std;
+
+int main(){
+  UnicodeString test = "appeltaart";
+  cout << test << endl;
+}
